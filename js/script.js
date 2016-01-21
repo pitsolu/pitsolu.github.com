cf432a38fe105f4673307a0a63d4edc871d5d31e
pitsolu <pitsolu@gmail.com>
2016-01-21 13:09:39 +0300

    custom validation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 (function() {
 
-    $(document).ready(function() {
+    function isValidEmailAddress(emailAddress) {
 
-        $("form").submit(function(e){
-
-            e.preventDefault();
-        });
-    });
+        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        return pattern.test(emailAddress);
+    };
 
     $.routr.add("home", function(id){
 
@@ -22,6 +20,78 @@
         $(".content-body .container .row").prepend($("#contact-us").html());
         $("#left-bar").hide();
         $("#main-nav").hide();
+
+        $("#send").click(function(){
+
+            if(!$("#name").val())
+                smoke.signal("Name field is required!", function(e){}, {
+
+                        duration: 3000,
+                        classname: "custom-class"
+                    });
+
+            else if(!$("#email").val())
+                smoke.signal("Email field is required!", function(e){}, {
+
+                        duration: 3000,
+                        classname: "custom-class"
+                    });
+
+            else if(!isValidEmailAddress($("#email").val()))
+                smoke.signal("Invalid email address!", function(e){}, {
+
+                        duration: 3000,
+                        classname: "custom-class"
+                    });
+
+            else if(!$("#subject").val())
+                smoke.signal("Subject field is required!", function(e){}, {
+
+                        duration: 3000,
+                        classname: "custom-class"
+                    });
+
+            else if(!$("#message").val())
+                smoke.signal("Message field is required!", function(e){}, {
+
+                        duration: 3000,
+                        classname: "custom-class"
+                    });
+            else
+                $.ajax({
+
+                    url: "http://formspree.io/pitsolu@gmail.com",
+                    method: "POST",
+                    data: {
+                            "name":$("#name").val(),
+                            "_replyto":$("#email").val(),
+                            "_subject":$("#subject").val(),
+                            "message":$("#message").val()
+                        },
+                    dataType: "json"
+                })
+                .success(function(){
+
+                    $("#name").val("");
+                    $("#email").val("");
+                    $("#subject").val("");
+                    $("#message").val("");
+
+                    smoke.signal("Thanks you feedback has been submitted.", function(e){}, {
+
+                        duration: 3000,
+                        classname: "custom-class"
+                    });
+                })
+                .fail(function(){
+
+                    smoke.signal("Something went wrong!!!", function(e){}, {
+                        
+                        duration: 3000,
+                        classname: "custom-class"
+                    });
+                });
+        });
     });
 
     $.routr.execute("home");
@@ -30,50 +100,6 @@
     $("#trigger-overlay").click(function(){
 
         $("#main-nav").show();
-    });
-
-    $("#send").click(function(e){
-
-        if(!!$("#name").val() &&
-            !!$("#email").val() &&
-            !!$("#subject").val() &&
-            !!$("#message").val()){
-
-            $.ajax({
-
-                url: "http://formspree.io/pitsolu@gmail.com",
-                method: "POST",
-                data: {
-                        "name":$("#name").val(),
-                        "_replyto":$("#email").val(),
-                        "_subject":$("#subject").val(),
-                        "message":$("#message").val()
-                    },
-                dataType: "json"
-            })
-            .success(function(){
-
-                $("#name").val("");
-                $("#email").val("");
-                $("#subject").val("");
-                $("#message").val("");
-
-                smoke.signal("Thanks you feedback has been submitted.", function(e){}, {
-
-                    duration: 3000,
-                    classname: "custom-class"
-                });
-            })
-            .fail(function(){
-
-                smoke.signal("Something went wrong!!!", function(e){}, {
-                    
-                    duration: 3000,
-                    classname: "custom-class"
-                });
-            });
-        }
-        // else console.log("Empty Form!!!");
     });
 
 })();
