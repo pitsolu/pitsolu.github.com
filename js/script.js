a6c23026ef190d36564a38826002779973a56620
pitsolu <pitsolu@gmail.com>
2016-02-22 15:56:10 +0300

    loading pulsate

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 (function() {
 
+    $.fn.pulse = function(options) {
+
+        var options = $.extend({
+            times: 3,
+            duration: 1000
+        }, options);
+
+        var period = function(callback) {
+            $(this).animate({opacity: 0}, options.duration, function() {
+                $(this).animate({opacity: 1}, options.duration, callback);
+            });
+        };
+        return this.each(function() {
+            var i = +options.times, self = this,
+            repeat = function() { --i && period.call(self, repeat) };
+            period.call(this, repeat);
+        });
+    };
+
     function isValidEmailAddress(emailAddress) {
 
         var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
@@ -13,6 +32,8 @@
         $("#left-bar").hide();
         $("#main-nav").hide();
 
+        $(".loading").pulse();
+
         $("img.lazy").lazyload({
 
             effect : "fadeIn"
@@ -20,7 +41,7 @@
 
         $(".gallery ul li a img").on("load", function() {
           
-            $(".loading").hide();
+            $(".loading").fadeOut("slow");
         })
     });
 
