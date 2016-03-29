7a44b8b59975841faf4a1a4750d7f50222bc6964
pitsolu <pitsolu@gmail.com>
2016-03-29 17:31:23 +0300

    added blog.js ref list, clean up

---
+++ blog/blog.js
@@ -0,0 +1,19 @@
+[
+	
+	{
+		"name":"201603220000"
+		"title":"How To Gain Traffic Through Blogging"
+		"author":"David Gabriel",
+		"tags":["blogging","contentmarketing","marketing","digitalmarketing","socialmedia"],
+		"source":"http://www.gabrieldigitalmarketing.com/blog-posts/how-to-gain-traffic-through-blogging"
+		"twitter":"@dgabrielsyr"
+	},
+	{
+		"name":"201603220001"
+		"title":"5 Reasons A Brand Story Is Your Most Valuable Marketing Tool"
+		"author":"Lisa Lacy",
+		"tags":["branding","marketing"],
+		"source":"(http://www.momentology.com/9526-brand-story/?utm_id=email28&utm_term=9526-brand-story&utm_content=buffer15933&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer"
+		"twitter":"@lisalacy"
+	}
+]
