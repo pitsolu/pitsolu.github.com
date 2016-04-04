414f58e71718f02d85d39e7a5089e53d48f424eb
pitsolu <pitsolu@gmail.com>
2016-04-04 16:28:39 +0300

    added david gabriel photo, try inview scroll - wip, resized pictures, added author footer in slides

--- blog/js/script.js
+++ blog/js/script.js
@@ -1,15 +1,30 @@
 (function(){
 
+	function isScrolledIntoView(elem){
+
+	    var $elem = $(elem);
+	    var $window = $(window);
+
+	    var docViewTop = $window.scrollTop();
+	    var docViewBottom = docViewTop + $window.height();
+
+	    var elemTop = $elem.offset().top;
+	    var elemBottom = elemTop + $elem.height();
+
+	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	}
+
 	function scrollTo(){
 
 		if(window.location.hash.trim()=="#bottom")
 			$("html, body").animate({ scrollTop: $(document).height() }, 900);
 		else
-			$('html, body').stop().animate({
-		        'scrollTop': $("a[href="+window.location.hash+"]").offset().top-55
-		    }, 900, 'swing', function () {
-		        //
-		    });
+			if(!!window.location.hash)
+				$('html, body').stop().animate({
+			        'scrollTop': $("a[href="+window.location.hash+"]").offset().top-55
+			    }, 900, 'swing', function () {
+			        //
+			    });
 	}
 
 	$(window).bind( 'hashchange', function(e){ 
@@ -23,6 +38,11 @@
 
 		if($(e).attr("href").substr(0,4) == "http")
 			$(e).attr("target","_blank");
+	});
+
+	$( window ).scroll(function() {
+	  
+		// console.log(isScrolledIntoView("a[href='#focus']"));
 	});
 
 	$(window).load(function() {
