ca568280f4c7aae7659caa686182ad981c6bcf2c
pitsolu <pitsolu@gmail.com>
2022-01-02 16:10:11 +0300

    styling and fixes

--- site/components/blogs/controller.js
+++ site/components/blogs/controller.js
@@ -1,7 +1,11 @@
 app.controller("blogsController", ["$scope", "$http", "$stateParams", "blogsService", 
 	function($scope, $http, $stateParams, blogsService){
 
-	$scope.blog_type = $stateParams.type;
+	$scope.blog_type = $stateParams.type
+
+	$scope.title = "Blogs";
+	if($stateParams.type == "dev")
+		$scope.title = "Dev Blogs"
 
 	blogsService.get($stateParams.type).then(function(data){
 
