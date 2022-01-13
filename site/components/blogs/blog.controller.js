b18cecec61da133eeba504b90ff3a3e2336b4813
pitsolu <pitsolu@gmail.com>
2022-01-13 22:49:40 +0300

    typo

--- site/components/blogs/blog.controller.js
+++ site/components/blogs/blog.controller.js
@@ -14,15 +14,20 @@ app.controller("blogController", ["$scope", "$http", "$state", "$stateParams", "
 
 		blogService.get(name.replace(".md",".json"), type).then(function(data){
 
+			// console.log("+++")
+
 			$scope.meta = $.extend(data, {
 
 				content_type:"text",
 				url:$location.$$absUrl,
-				image:location.$$host.concat("/images/logo-small.png"),
-				large_image:location.$$host.concat("/images/logo-large.png"),
+				image:$location.$$host.concat("/images/logo-small.png"),
+				large_image:$location.$$host.concat("/images/logo-large.png"),
 				descr:"",
 				alt_text_img:"pitsolu logo",
 				fb_app_id:""
 			})
+
+			// console.log("---")
+			// console.log($scope.meta)
 		})
 }]);
